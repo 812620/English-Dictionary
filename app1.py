030046c100f82dcb6efa8779208e87892988fdfe
Anjali Joshi <33463218+812620@users.noreply.github.com>
2018-08-12 14:49:06 +0530

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,24 @@
+import json
+from difflib import get_close_matches
+
+data=json.load(open("data.json"))
+
+def dic(word):
+    word = word.lower()
+    if word in data:
+        return data[word]
+    elif len(get_close_matches(word,data.keys()))> 0:
+        yn= input("Did you mean %s instead? Enter Y for yes,or N for No: " % get_close_matches(word, data.keys())[0])
+        if yn=="Y":
+            return data[get_close_matches(word,data.keys())[0]]
+        elif yn=="N":
+            return "The word doesn't exit.Please enter again.."
+        else:
+            return "The word doesn't exit..."
+    else:
+       return "The word doen't exit. Please recheck the word "
+
+
+word=input("Enter word: ")
+
+print(dic(word))
